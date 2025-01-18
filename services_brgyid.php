b8213ef1f064250553cc73b36954f36c3adb322b
Kurt013 <kurtvincentalmodovar65@gmail.com>
2025-01-19 05:06:50 +0800

    added title and logo on each tab

--- services_brgyid.php
+++ services_brgyid.php
@@ -15,12 +15,11 @@
 ?>
 
 
-
 <!DOCTYPE html>
-
 <html>
   <head> 
-    <title> Barangay Management System </title>
+  <title> Barangay Sinalhan | Barangay ID </title>
+  <link rel="icon" href="./assets/sinlogo.png" type="image/x-icon">
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous"></script>
       <!-- responsive tags for screen compatibility -->
--- services_business.php
+++ services_business.php
@@ -21,7 +21,8 @@
 
 <html>
   <head> 
-    <title> Barangay Management System </title>
+  <title> Barangay Sinalhan | Business Permit </title>
+  <link rel="icon" href="./assets/sinlogo.png" type="image/x-icon">
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-modal/2.2.6/js/bootstrap-modalmanager.min.js" integrity="sha512-/HL24m2nmyI2+ccX+dSHphAHqLw60Oj5sK8jf59VWtFWZi9vx7jzoxbZmcBeeTeCUc7z1mTs3LfyXGuBU32t+w==" crossorigin="anonymous"></script>
       <script src="./js-components/component-js-custompurpose.js"></script>
